87a5f2636c10e80f5db8775d2f41bc70a98c41fe
buger815 <buger815@gmail.com>
2023-05-07 19:30:34 +0900

    UI Connect

---
+++ Assets/02_Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    public TextMeshProUGUI HP_Txt;
+    public TextMeshProUGUI Gold_Txt;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        HP_Txt.text = $"x{GameManager.instance._base.HP}";
+        Gold_Txt.text = $"x{GameManager.instance.money}";   
+    }
+
+
+
+
+}

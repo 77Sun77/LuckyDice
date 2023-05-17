7c5c1966bec2cd12397b80cec7ace363ba01bdee
redflag-HSH <66863292+redflag-HSH@users.noreply.github.com>
2023-05-17 22:18:30 +0900

    dialog System added
    Still in progress.
    code was constructed for other game so it should be adjusted later

---
+++ Assets/02_Scripts/Dialog/dialogCore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "dialogCore", menuName = "ScriptableObject/dialogCore", order = int.MaxValue)]
+public class dialogCore : ScriptableObject
+{
+    public string[] names;
+    public int[] talker;
+    //[TextArea()]
+    public string[] sentences;
+}

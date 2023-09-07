4c27eb5c6c64d0dfc17edc79e960ce4d2a886ded
buger815 <buger815@gmail.com>
2023-09-07 18:03:14 +0900

    Fix Calculating DiceValue

--- Assets/02_Scripts/04_Dice/DiceControl.cs
+++ Assets/02_Scripts/04_Dice/DiceControl.cs
@@ -12,6 +12,7 @@ public class DiceControl : MonoBehaviour
     public float Value_A, Value_B;
 
     public bool valueUp, enable , IsRollingDice;
+    public bool IsDebugMode;
     public GameObject temp;
     void OnEnable()
     {
@@ -23,7 +24,7 @@ public class DiceControl : MonoBehaviour
 
     void Update()
     {
-        if (enable)
+        if (enable && !IsDebugMode)
         {
             #region
             //fillImage.fillAmount = value;
@@ -80,34 +81,27 @@ public class DiceControl : MonoBehaviour
         IsRollingDice = true;
         int number = 0;
 
-        if (value < 0.1428571f)
+        if (value < 0.2f)
         {
-            number = 1;
+            number = ValueCalculator(value, 0.2f, 0.0f, 1, 2);
         }
-        else if (value < 0.2857142f) // 0.1428571 ~ 0.2857142
+        else if (value < 0.4f) 
         {
-            number = ValueCalculator(value, 0.2857142f, 0.1428571f, 1, 2);
+            number = ValueCalculator(value, 0.4f, 0.2f, 2, 3);
         }
-        else if (value < 0.4285713f) // 0.2857142f ~ 0.4285713
+        else if (value < 0.6f)
         {
-            number = ValueCalculator(value, 0.4285713f, 0.2857142f, 2, 3);
+            number = ValueCalculator(value, 0.6f, 0.4f, 3, 4);
         }
-        else if (value < 0.5714284f) // 0.4285713 ~ 0.5714284
+        else if (value < 0.8f) 
         {
-            number = ValueCalculator(value, 0.5714284f, 0.4285713f, 3, 4);
+            number = ValueCalculator(value, 0.8f, 0.6f, 4, 5);
         }
-        else if (value < 0.7142855f) // 0.5714284 ~ 0.7142855
+        else if (value < 1f) 
         {
-            number = ValueCalculator(value, 0.7142855f, 0.5714284f, 4, 5);
+            number = ValueCalculator(value, 1f, 0.8f, 5, 6);
         }
-        else if (value < 0.8571426f) // 0.7142855 ~ 0.8571426
-        {
-            number = ValueCalculator(value, 0.8571426f, 0.7142855f, 5, 6);
-        }
-        else
-        {
-            number = 6;
-        }
+        
         DiceManager.instance.DiceControl(number, DiceRotation.DIceKind.Ally);
         Debug.Log($"Value is {value},DiceNumber is {number}");
         //gameObject.SetActive(false);
@@ -145,13 +139,15 @@ public class DiceControl : MonoBehaviour
     int ValueCalculator(float value, float max, float min, int blink1, int blink2) // value, ÃÖ´ñ°ª, ÃÖ¼Ú°ª, ÃÖ¼Ò ´«²û, ÃÖ´ë ´«²û
     {
         value -= min;
-        max -= min;
 
-        float random = Random.Range(0, max);
+        float random = Random.Range(0, 0.2f);
+        Debug.Log($"Random Value is {random}");
+
         if (random <= value)
         {
-            return blink1;
+            return blink2;
         }
-        return blink2;
+        return blink1;
+        
     }
 }
